3d6b6d44f8f8e26d310460944792ce3f06a21c99
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2024-01-11 23:18:21 +0500

    Add controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :groups
   resources :entities
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "splashes/index"
+  unauthenticated do
+    root 'splashes#index'
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
 end

afc7a7c5173c6496508126ae8db9f5fa83c94e7a
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2024-01-11 22:46:08 +0500

    Add migration files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, class_name: 'Group', dependent: :destroy, foreign_key: 'user_id'
+  has_many :entities, class_name: 'Entitiy', dependent: :destroy, foreign_key: 'user_id'
+  validates :name, presence: true
 end
